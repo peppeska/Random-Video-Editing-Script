61d687afc88b1d6849c3127310de9e7009cebf8c
Giuseppe Moscato <giuseppe.moscato@icn.it>
2015-09-08 09:55:55 +0200

    add shortest to ffmpeg command

--- mkvideo.sh
+++ mkvideo.sh
@@ -3,7 +3,7 @@ mkdir editing
 rm -rf editing/*
 files=(./*)
 seconds=2
-for ((I=0;I<125;I++))
+for ((I=0;I<200;I++))
 do
 	video=${files[RANDOM % ${#files[@]}]}
 	if [[ $video == *GOPR* ]]
@@ -23,4 +23,4 @@ rm output.MP4
 mencoder -ovc copy -oac pcm editing/*.MP4 -o output.MP4
 
 rm output_final.MP4
-ffmpeg -i output.MP4 -i music.mp3 -map 0:0 -map 1:0 output_final.MP4
+ffmpeg -i output.MP4 -i music.mp3 -map 0:0 -map 1:0 -shortest output_final.MP4
