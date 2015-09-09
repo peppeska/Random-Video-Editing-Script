b3e3216a8fb9eedefff4e799b6ca798ca8eb278d
Giuseppe Moscato <giuseppe.moscato@icn.it>
2015-09-09 09:48:20 +0200

    suppress output ffmpeg, more clear

--- mkvideo.sh
+++ mkvideo.sh
@@ -3,19 +3,19 @@ mkdir editing
 rm -rf editing/*
 files=(./*)
 seconds=2
-for ((I=0;I<200;I++))
+for ((I=0;I<350;I++))
 do
 	video=${files[RANDOM % ${#files[@]}]}
 	if [[ $video == *GOPR* ]]
 	then
-		echo $video
+		echo "=="
+		echo "File: " $video
 		durata=`ffmpeg -i $video 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }' `
-		echo $durata
+		echo "duration: "$durata
 		end=`shuf -i $seconds-$durata -n 1`
 		start=$((end-$seconds))
-		echo START $start , END $end
-		command=`ffmpeg -i $video -ss $start -t $seconds -strict -2 editing/$I.MP4`
-		#echo $command
+		echo start: $start , end: $end
+		command=`ffmpeg -loglevel panic -i $video -ss $start -t $seconds -strict -2 editing/$I.MP4`
 	fi
 done
 
